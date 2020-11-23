66f5ab8ec3891316ff72f698c9478db17a09542c
Pedro Alves <pedro22carro@gmail.com>
2020-11-23 16:13:36 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace idade_para_votar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o ano atual");
+            int ano = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o seu ano de nascimento");
+            int nascimento = int.Parse(Console.ReadLine());
+            int idade = ano - nascimento;
+            if(idade >= 16){
+                Console.WriteLine($"Você pode votar este ano, pois você tem {idade} anos.");
+            }else{
+                Console.WriteLine($"Você não pode votar este ano, pois você é menor de 16 anos.");
+            }
+            
+        }
+    }
+}
